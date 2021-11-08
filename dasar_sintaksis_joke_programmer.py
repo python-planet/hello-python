0f816389113f48fbc4b9d3c2652b071214a38337
moohat <refixzcorp@gmail.com>
2021-11-08 07:46:48 -0800

    my first python code to commit

---
+++ dasar_sintaksis_joke_programmer.py
@@ -0,0 +1,8 @@
+"""
+semua sintaksis dasar bahasapemrograman terdiri dari:
+1. Sekuensial : langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+print('Ibu berkata, "pergi ketoko"')
+print("My name is Taufik")
--- main.py
+++
@@ -1,5 +0,0 @@
-"""
-Ini adalah demo project pertama saya dengan python
-"""
-print("Hello world")
-print("My name is Taufik")
